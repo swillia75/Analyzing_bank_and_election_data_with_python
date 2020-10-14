3090de38bf72a3188124f70a29b4545e15c09889
Shawn Williams <swillia75@gmail.com>
2020-10-14 14:56:02 -0400

    cleaned comments

--- PyBank/PyBank_main.py
+++ PyBank/PyBank_main.py
@@ -1,45 +1,52 @@
 #import os
+
 import os
 
 #import csv to read file
+
 import csv
 
-#define variables
-
-
 # start counters
+
 total=0
 max_profit = 0
 max_losses = 0
+
 # Path to collect data from the Resources folder
+
 PyBank_csv = os.path.join("Resources","budget_data.csv")
 
 
 # Open and read the CSV
+
 with open(PyBank_csv, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
     #reader header
+
     csv_header = next(csvfile)
 
     #create list
+
     budget_list = list(csvreader)
 
     
-     #Calculate Total months 
+    #Calculate Total months 
+
     total_months = len(budget_list) 
 
-    #The net total amount "profits/losses" over entire period
-    for row in budget_list:
+        #The net total amount "profits/losses" over entire period
+    
         
         total += int(row[1])
         total_formatted = "${:.0f}".format(total)
        
-    #average change for "profits/losses" over entire period
+        #average change for "profits/losses" over entire period
+
         average_change = (total / total_months)
         average_change_formatted = "${:.2f}".format(average_change)
 
-#The greatest increase in profits (date and amount) over entire period
+        #The greatest increase in profits (date and amount) over entire period
     
         if (int(row[1])) > max_profit:
             max_profit = int(row[1])
@@ -48,7 +55,7 @@ with open(PyBank_csv, newline="") as csvfile:
 
          
 
-#The greatest decrease in losses (date and amount) over entire period
+        #The greatest decrease in losses (date and amount) over entire period
 
         elif (int(row[1])) < max_losses:
             max_losses = int(row[1])
